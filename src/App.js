f8e6868585972d77526e757aab9a88dcd8f5ca01
r3X <rch246chauhan@gmail.com>
2022-05-16 10:38:37 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import styles from "./App.module.css";
 import Header from "./components/Layout/Header";
 import Menu from "./components/Menu/Menu";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./components/store/CartProvider";
 
 function App() {
+  const [cartVisibility, setCartVisibility] = useState(false);
 
-const [cartVisibility,setCartVisibility]=useState(false)
+  const visibleCartHandler = () => {
+    setCartVisibility(true);
+  };
+  const invisibleCartHandler = () => {
+    setCartVisibility(false);
+  };
 
-const visibleCartHandler=()=>{
-  setCartVisibility(true)
-}
-const invisibleCartHandler=()=>{
-  setCartVisibility(false)
-}
-  
   return (
     // <div className="App">
-    <React.Fragment>
-      {cartVisibility&&<Cart onClose={invisibleCartHandler}></Cart>}
+    <CartProvider>
+      {cartVisibility && <Cart onClose={invisibleCartHandler}></Cart>}
       <Header onVisibleCart={visibleCartHandler}></Header>
       <main>
         <Menu></Menu>
       </main>
-    </React.Fragment>
+    </CartProvider>
   );
 }
 
